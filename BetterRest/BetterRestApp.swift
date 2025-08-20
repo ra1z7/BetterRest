d5cec0b457e739b8d3d11d9fb04f744dbc68b90b
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-20 14:45:28 +0545

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Purnaman Rai (College) on 20/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
