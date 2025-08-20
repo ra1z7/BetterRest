6867c3df76a9d087e42f94c3e18de2367d88c08e
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-20 19:27:58 +0545

    improve UI/UX

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,7 +9,7 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
 
@@ -17,39 +17,57 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
 
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+
     var body: some View {
         NavigationStack {
             Form {
                 Section {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    DatePicker(
-                        "Please select a time",
-                        selection: $wakeUp,
-                        displayedComponents: .hourAndMinute
-                    )
-                    .labelsHidden()
+                    HStack {
+                        Text("When do you want to wake up?")
+                            .font(.headline)
+
+                        Spacer()
+
+                        DatePicker(
+                            "Please select a time",
+                            selection: $wakeUp,
+                            displayedComponents: .hourAndMinute
+                        )
+                        .labelsHidden()
+                    }
                 }
 
                 Section {
-                    Text("What's your desired amount of sleep?")
-                        .font(.headline)
-                    Stepper(
-                        "\(sleepAmount.formatted()) hours",
-                        value: $sleepAmount,
-                        in: 4...12,
-                        step: 0.25
-                    )
+                    VStack(alignment: .leading) {
+                        Text("What's your desired amount of sleep?")
+                            .font(.headline)
+                        Stepper(
+                            "\(sleepAmount.formatted()) hours",
+                            value: $sleepAmount,
+                            in: 4...12,
+                            step: 0.25
+                        )
+                    }
                 }
 
                 Section {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    Stepper(
-                        "\(coffeeAmount) cup(s)",
-                        value: $coffeeAmount,
-                        in: 1...20
-                    )
+                    VStack(alignment: .leading) {
+                        Text("Daily coffee intake")
+                            .font(.headline)
+                        // Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                        Stepper(
+                            "^[\(coffeeAmount) cup](inflect: true)",
+                            value: $coffeeAmount,
+                            in: 1...20
+                        )
+                        //  This syntax tells SwiftUI that the word "cup" needs to be inflected to match whatever is in the coffeeAmount variable, which in this case means it will automatically be converted from "cup" to "cups" as appropriate.
+                    }
                 }
             }
             .navigationTitle("BetterRest")
