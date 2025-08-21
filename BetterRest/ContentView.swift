3343f6c98b29c1f2084b1c415ffb5b68cabc7942
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-21 18:31:30 +0545

    implement challenge 1

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -27,47 +27,31 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             Form {
-                Section {
-                    HStack {
-                        Text("When do you want to wake up?")
-                            .font(.headline)
-
-                        Spacer()
-
-                        DatePicker(
-                            "Please select a time",
-                            selection: $wakeUp,
-                            displayedComponents: .hourAndMinute
-                        )
-                        .labelsHidden()
-                    }
+                Section("When do you want to wake up?") {
+                    DatePicker(
+                        "Please select a time",
+                        selection: $wakeUp,
+                        displayedComponents: .hourAndMinute
+                    )
                 }
 
-                Section {
-                    VStack(alignment: .leading) {
-                        Text("What's your desired amount of sleep?")
-                            .font(.headline)
-                        Stepper(
-                            "\(sleepAmount.formatted()) hours",
-                            value: $sleepAmount,
-                            in: 4...12,
-                            step: 0.25
-                        )
-                    }
+                Section("your desired amount of sleep?") {
+                    Stepper(
+                        "\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount,
+                        in: 4...12,
+                        step: 0.25
+                    )
                 }
 
-                Section {
-                    VStack(alignment: .leading) {
-                        Text("Daily coffee intake")
-                            .font(.headline)
-                        // Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-                        Stepper(
-                            "^[\(coffeeAmount) cup](inflect: true)",
-                            value: $coffeeAmount,
-                            in: 1...20
-                        )
-                        //  This syntax tells SwiftUI that the word "cup" needs to be inflected to match whatever is in the coffeeAmount variable, which in this case means it will automatically be converted from "cup" to "cups" as appropriate.
-                    }
+                Section("Daily coffee intake") {
+                    // Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Stepper(
+                        "^[\(coffeeAmount) cup](inflect: true)",
+                        value: $coffeeAmount,
+                        in: 1...20
+                    )
+                    //  This syntax tells SwiftUI that the word "cup" needs to be inflected to match whatever is in the coffeeAmount variable, which in this case means it will automatically be converted from "cup" to "cups" as appropriate.
                 }
             }
             .navigationTitle("BetterRest")
